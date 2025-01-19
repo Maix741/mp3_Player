490a48ade5d0bafb80e71bb52ed410b95d955d1a
Maix741 <maxi2018milian@outlook.de>
2025-01-19 14:33:16 +0100

    finished translations

--- src/locales/en_US.ts
+++ src/locales/en_US.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
 <context>
     <name>Mp3Player</name>
     <message>
         <location filename="../mp3_player.py" line="43"/>
         <source>MP3 Player</source>
-        <translation type="unfinished"></translation>
+        <translation>Mp3 Player</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="94"/>
         <source>Song: {self.current_music}</source>
-        <translation type="unfinished"></translation>
+        <translation>Song: {self.current_music}</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="174"/>
         <source>Volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Volume</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="186"/>
         <source>Controls</source>
-        <translation type="unfinished"></translation>
+        <translation>Controls</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="202"/>
@@ -30,128 +30,128 @@
         <location filename="../mp3_player.py" line="411"/>
         <location filename="../mp3_player.py" line="468"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Play</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="206"/>
         <location filename="../mp3_player.py" line="397"/>
         <source>Play Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Play playlist</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="211"/>
         <source>Stop</source>
-        <translation type="unfinished"></translation>
+        <translation>Stop</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="216"/>
         <location filename="../mp3_player.py" line="444"/>
         <source>Loop</source>
-        <translation type="unfinished"></translation>
+        <translation>Loop</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="221"/>
         <source>Save Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Save playlist</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="226"/>
         <source>Add from Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Add from folder</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="262"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Delete</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="302"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Remove</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="303"/>
         <source>Add to playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Add to playlist</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="324"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>File</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="328"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Open</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="332"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Exit</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="336"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation>Clear</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="356"/>
         <location filename="../mp3_player.py" line="367"/>
         <source>Stop Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Stop playlist</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="368"/>
         <location filename="../mp3_player.py" line="420"/>
         <location filename="../mp3_player.py" line="550"/>
         <source>Pause</source>
-        <translation type="unfinished"></translation>
+        <translation>Pause</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="385"/>
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>None</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="391"/>
         <source>Song: {display_name}</source>
-        <translation type="unfinished"></translation>
+        <translation>Song: {display_name}</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="452"/>
         <source>Unloop</source>
-        <translation type="unfinished"></translation>
+        <translation>Unloop</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="477"/>
         <source>Choose Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Choose playlist</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="501"/>
         <source>Enter Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Enter name</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="501"/>
         <location filename="../mp3_player.py" line="515"/>
         <source>Please enter the name of the Playlist:</source>
-        <translation type="unfinished"></translation>
+        <translation>Please enter the name of the playlist:</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="515"/>
         <source>Enter Playlist Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Enter playlist name</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="525"/>
         <source>Select MP3 Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Select audio files</translation>
     </message>
     <message>
         <location filename="../mp3_player.py" line="525"/>
         <source>audio (*.mp3 *.wav *.ogg *.flac);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>audio (*.mp3 *.wav *.ogg *.flac);;all files(*)</translation>
     </message>
 </context>
 </TS>
